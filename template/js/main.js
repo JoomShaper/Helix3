6089cdf2197ca23f3067a9b5192570f1c44a76c0
nayem <git@joomshaper.com>
2015-02-26 16:53:04 +0600

    ajax rating js added

--- template/js/main.js
+++ template/js/main.js
@@ -5,6 +5,7 @@
 * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or later
 */
 jQuery(function($) {
+
     $('#offcanvas-toggler').on('click', function(event){
         event.preventDefault();
         $('body').toggleClass('offcanvas');
@@ -23,12 +24,62 @@ jQuery(function($) {
 
     //Sticky Menu
     $(document).ready(function(){
-    	$("body.sticky-header").find('#sp-header').sticky({topSpacing:0})
+        $("body.sticky-header").find('#sp-header').sticky({topSpacing:0})
     });
 
     //Tooltip
     $(function () {
       $('[data-toggle="tooltip"]').tooltip()
+    });
+
+    $(document).on('click', '.sp-rating .star', function(event) {
+        event.preventDefault();
+
+        var data = {
+            'action':'voting',
+            'user_rating' : $(this).data('number'),
+            'id' : $(this).closest('.post_rating').attr('id')
+        };
+
+        var request = {
+                'option' : 'com_ajax',
+                'plugin' : 'helix3',
+                'data'   : data,
+                'format' : 'json'
+            };
+
+        $.ajax({
+            type   : 'POST',
+            data   : request,
+            beforeSend: function(){
+                $('.post_rating .ajax-loader').show();
+            },
+            success: function (response) {
+                var data = $.parseJSON(response.data);
+
+                $('.post_rating .ajax-loader').hide();
+
+                if (data.status == 'invalid') {
+                    $('.post_rating .voting-result').text('You have already rated this entry!').fadeIn('fast');
+                }else if(data.status == 'false'){
+                    $('.post_rating .voting-result').text('Somethings wrong here, try again!').fadeIn('fast');
+                }else if(data.status == 'true'){
+                    var rate = data.action;
+                    $('.voting-symbol').find('.star').each(function(i) {
+                        if (i < rate) {
+                           $( ".star" ).eq( -(i+1) ).addClass('active');
+                        }
+                    });
+
+                    $('.post_rating .voting-result').text('Thank You!').fadeIn('fast');
+                }
+
+            },
+            error: function(){
+                $('.post_rating .ajax-loader').hide();
+                $('.post_rating .voting-result').text('Failed to rate, try again!').fadeIn('fast');
+            }
+        });
     });
 
 });

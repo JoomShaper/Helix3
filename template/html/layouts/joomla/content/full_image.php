a46f8e6a772543d751b3b8c1065791d3007abc15
JoomShaper <support@joomshaper.com>
2015-11-02 13:34:36 +0600

    Featured image in frontend

--- template/html/layouts/joomla/content/full_image.php
+++ template/html/layouts/joomla/content/full_image.php
@@ -9,14 +9,22 @@
 
 defined('_JEXEC') or die;
 $params  = $displayData->params;
-$images = json_decode($displayData->images);
+$attribs 		= json_decode($displayData->attribs);
+$images 		= json_decode($displayData->images);
+$full_image 	= '';
+
+if(isset($attribs->spfeatured_image) && $attribs->spfeatured_image != '') {
+	$full_image = $attribs->spfeatured_image;
+} elseif(isset($images->image_fulltext) && !empty($images->image_fulltext)) {
+	$full_image = $images->image_fulltext;
+}
 ?>
 
-<?php if (isset($images->image_fulltext) && !empty($images->image_fulltext)) : ?>
+<?php if(!empty($full_image) || (isset($images->image_fulltext) && !empty($images->image_fulltext))) { ?>
 	<?php $imgfloat = (empty($images->float_fulltext)) ? $params->get('float_fulltext') : $images->float_fulltext; ?>
 	<div class="entry-image full-image"> <img
 		<?php if ($images->image_fulltext_caption):
 		echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_fulltext_caption) . '"';
 		endif; ?>
-		src="<?php echo htmlspecialchars($images->image_fulltext); ?>" alt="<?php echo htmlspecialchars($images->image_fulltext_alt); ?>" itemprop="image"/> </div>
+		src="<?php echo htmlspecialchars($full_image); ?>" alt="<?php echo htmlspecialchars($images->image_fulltext_alt); ?>" itemprop="image"/> </div>
 <?php endif; ?>
--- template/html/layouts/joomla/content/intro_image.php
+++ template/html/layouts/joomla/content/intro_image.php
@@ -8,22 +8,45 @@
  */
 
 defined('_JEXEC') or die;
-$params  = $displayData->params;
+$tplParams 		= JFactory::getApplication()->getTemplate(true)->params;
+$params  		= $displayData->params;
+$attribs 		= json_decode($displayData->attribs);
+$images 		= json_decode($displayData->images);
+$imgsize 		= $tplParams->get('blog_list_image', 'default');
+$intro_image 	= '';
+
+if(isset($attribs->spfeatured_image) && $attribs->spfeatured_image != '') {
+
+	if($imgsize == 'default') {
+		$intro_image = $attribs->spfeatured_image;
+	} else {
+		$intro_image = $attribs->spfeatured_image;
+		$basename = basename($intro_image);
+		$list_image = JPATH_ROOT . '/' . dirname($intro_image) . '/' . JFile::stripExt($basename) . '_'. $imgsize .'.' . JFile::getExt($basename);
+		if(file_exists($list_image)) {
+			$intro_image = JURI::root(true) . '/' . dirname($intro_image) . '/' . JFile::stripExt($basename) . '_'. $imgsize .'.' . JFile::getExt($basename);
+		}
+	}
+} elseif(isset($images->image_intro) && !empty($images->image_intro)) {
+	$intro_image = $images->image_intro;
+}
 ?>
-<?php $images = json_decode($displayData->images); ?>
-<?php if (isset($images->image_intro) && !empty($images->image_intro)) : ?>
-	<div class="entry-image intro-image"> 
-	<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
-	<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>"><img
+
+<?php if(!empty($intro_image) || (isset($images->image_intro) && !empty($images->image_intro))) { ?>
+
+<div class="entry-image intro-image">
+	<?php if ($params->get('link_titles') && $params->get('access-view')) { ?>
+		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>">
+	<?php } ?>
+
+	<img
 	<?php if ($images->image_intro_caption):
-		echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_intro_caption) . '"';
-	endif; ?>
-	src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>" itemprop="thumbnailUrl"/></a>
-	<?php else : ?><img
-	<?php if ($images->image_intro_caption):
-		echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_intro_caption) . '"';
-	endif; ?>
-	src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>" itemprop="thumbnailUrl"/>
-	<?php endif; ?> 
+			echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_intro_caption) . '"';
+			endif; ?>
+	src="<?php echo htmlspecialchars($intro_image); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>" itemprop="thumbnailUrl"/>
+
+	<?php if ($params->get('link_titles') && $params->get('access-view')) { ?>
+		</a>
+	<?php } ?>
 </div>
-<?php endif; ?>
+<?php } ?>

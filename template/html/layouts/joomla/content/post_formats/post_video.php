da58f7d4ff8396859429bc52d22fbf06642d222f
Rifat Wahid Alif <rifat3280@gmail.com>
2016-08-04 17:09:34 +0600

    Update version 1.3
    - improved Template Security.
    - Facebook like issue in articles fixed.

--- template/html/layouts/joomla/content/post_formats/post_video.php
+++ template/html/layouts/joomla/content/post_formats/post_video.php
@@ -34,10 +34,20 @@ if ( $displayData['params']->get('video') ) {
 
 	if($video_src) {
 		?>
+
 		<div class="entry-video embed-responsive embed-responsive-16by9">
-			<iframe class="embed-responsive-item" src="<?php echo $video_src; ?>" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+			<object class="embed-responsive-item" style="width:100%;height:100%;" data="<?php echo $video_src; ?>">
+				<param name="movie" value="<?php echo $video_src; ?>">
+				<param name="wmode" value="transparent" />
+				<param name="allowFullScreen" value="true">
+				<param name="allowScriptAccess" value="always"></param>
+				<embed src="<?php echo $video_src; ?>"
+		         type="application/x-shockwave-flash" allowscriptaccess="always">
+		         </embed>
+			</object>
 		</div>
+
 		<?php
-	}
+	} // has video source
 	
-}
+} // has video value
--- template/html/layouts/joomla/content/social_share/social/facebook.php
+++ template/html/layouts/joomla/content/social_share/social/facebook.php
@@ -7,4 +7,15 @@
 //no direct access
 defined('_JEXEC') or die('Restricted Access');
 ?>
-<li><iframe src="//www.facebook.com/plugins/like.php?href=<?php echo $displayData['url']; ?>&amp;layout=button_count&amp;show_faces=false&amp;width=105&amp;action=like&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:105px; height:21px;" allowTransparency="true"></iframe></li>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-like" data-href="<?php echo $displayData['url']; ?>" data-width="75" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>
+

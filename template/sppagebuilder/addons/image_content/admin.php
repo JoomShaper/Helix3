827d2af27e2bfe0fe30536357a3681105c11bdd0
Nayem Majhar <nayemmajhar@gmail.com>
2017-01-27 18:01:42 +0600

    Image content addon in helix3 updated

--- template/sppagebuilder/addons/image_content/admin.php
+++ template/sppagebuilder/addons/image_content/admin.php
@@ -1,55 +1,63 @@
 <?php
 /**
- * @package SP Page Builder
- * @author JoomShaper http://www.joomshaper.com
- * @copyright Copyright (c) 2010 - 2015 JoomShaper
- * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or later
+* @package SP Page Builder
+* @author JoomShaper http://www.joomshaper.com
+* @copyright Copyright (c) 2010 - 2016 JoomShaper
+* @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or later
 */
 //no direct accees
-defined ('_JEXEC') or die ('resticted aceess');
+defined ('_JEXEC') or die ('restricted aceess');
 
 SpAddonsConfig::addonConfig(
-	array( 
-		'type'=>'content',
-		'addon_name'=>'sp_image_content',
-		'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_IMAGE_CONTENT'),
-		'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_IMAGE_CONTENT_DESC'),
-		'attr'=>array(
+array(
+	'type'=>'content',
+	'addon_name'=>'sp_image_content',
+	'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_IMAGE_CONTENT'),
+	'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_IMAGE_CONTENT_DESC'),
+	'attr'=>array(
+		'general' => array(
+
+			'admin_label'=>array(
+				'type'=>'text',
+				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_ADMIN_LABEL'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_ADMIN_LABEL_DESC'),
+				'std'=> ''
+			),
 
 			'separator1'=>array(
-				'type'=>'separator', 
+				'type'=>'separator',
 				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_IMAGE_CONTENT_IMAGE')
-				),
+			),
 
 			'image'=>array(
-				'type'=>'media', 
+				'type'=>'media',
 				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_IMAGE_CONTENT_IMAGE'),
-				),
-			
+			),
+
 			'image_alignment'=>array(
-				'type'=>'select', 
+				'type'=>'select',
 				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_IMAGE_CONTENT_IMAGE_ALIGNMENT'),
 				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_IMAGE_CONTENT_IMAGE_ALIGNMENT_DESC'),
 				'values'=>array(
-					'left'=>JText::_('COM_SPPAGEBUILDER_ADDON_GLOBAL_LEFT'),
-					'right'=>JText::_('COM_SPPAGEBUILDER_ADDON_GLOBAL_RIGHT'),
-					),
-				'std'=>'left',
+					'left'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_LEFT'),
+					'right'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_RIGHT'),
 				),
+				'std'=>'left',
+			),
 
 			'separator2'=>array(
-				'type'=>'separator', 
+				'type'=>'separator',
 				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_IMAGE_CONTENT_CONTENT')
-				),
+			),
 
 			'title'=>array(
-				'type'=>'text', 
+				'type'=>'text',
 				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_IMAGE_CONTENT_TITLE'),
 				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_IMAGE_CONTENT_TITLE_DESC'),
-				),
+			),
 
 			'heading_selector'=>array(
-				'type'=>'select', 
+				'type'=>'select',
 				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_HEADINGS'),
 				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_HEADINGS_DESC'),
 				'values'=>array(
@@ -59,76 +67,177 @@ SpAddonsConfig::addonConfig(
 					'h4'=>JText::_('COM_SPPAGEBUILDER_ADDON_HEADINGS_H4'),
 					'h5'=>JText::_('COM_SPPAGEBUILDER_ADDON_HEADINGS_H5'),
 					'h6'=>JText::_('COM_SPPAGEBUILDER_ADDON_HEADINGS_H6'),
-					),
+				),
 				'std'=>'h3',
+				'depends'=>array(array('title', '!=', '')),
 			),
 
 			'title_fontsize'=>array(
-				'type'=>'number', 
+				'type'=>'number',
 				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_TITLE_FONT_SIZE'),
 				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_TITLE_FONT_SIZE_DESC'),
-				'std'=>''
+				'std'=>'',
+				'depends'=>array(array('title', '!=', '')),
+			),
+
+			'title_lineheight'=>array(
+				'type'=>'text',
+				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_TITLE_LINE_HEIGHT'),
+				'std'=>'',
+				'depends'=>array(array('title', '!=', '')),
+			),
+
+			'title_fontstyle'=>array(
+				'type'=>'select',
+				'title'=> JText::_('COM_SPPAGEBUILDER_ADDON_TITLE_FONT_STYLE'),
+				'values'=>array(
+					'underline'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_UNDERLINE'),
+					'uppercase'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_UPPERCASE'),
+					'italic'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_ITALIC'),
+					'lighter'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_LIGHTER'),
+					'normal'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_NORMAL'),
+					'bold'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_BOLD'),
+					'bolder'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_BOLDER'),
 				),
+				'multiple'=>true,
+				'std'=>'',
+				'depends'=>array(array('title', '!=', '')),
+			),
+
+			'title_letterspace'=>array(
+				'type'=>'select',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_LETTER_SPACING'),
+				'values'=>array(
+					'0'=> 'Default',
+					'1px'=> '1px',
+					'2px'=> '2px',
+					'3px'=> '3px',
+					'4px'=> '4px',
+					'5px'=> '5px',
+					'6px'=>	'6px',
+					'7px'=>	'7px',
+					'8px'=>	'8px',
+					'9px'=>	'9px',
+					'10px'=> '10px'
+				),
+				'std'=>'0',
+				'depends'=>array(array('title', '!=', '')),
+			),
 
 			'title_fontweight'=>array(
-				'type'=>'text', 
+				'type'=>'text',
 				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_TITLE_FONT_WEIGHT'),
 				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_TITLE_FONT_WEIGHT_DESC'),
-				'std'=>''
-				),
+				'std'=>'',
+				'depends'=>array(array('title', '!=', '')),
+			),
 
 			'title_text_color'=>array(
 				'type'=>'color',
 				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_TITLE_TEXT_COLOR'),
 				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_TITLE_TEXT_COLOR_DESC'),
-				),
+				'depends'=>array(array('title', '!=', '')),
+			),
 
 			'title_margin_top'=>array(
 				'type'=>'number',
 				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_TITLE_MARGIN_TOP'),
 				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_TITLE_MARGIN_TOP_DESC'),
 				'placeholder'=>'10',
-				),
+				'depends'=>array(array('title', '!=', '')),
+			),
 
 			'title_margin_bottom'=>array(
 				'type'=>'number',
 				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_TITLE_MARGIN_BOTTOM'),
 				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_TITLE_MARGIN_BOTTOM_DESC'),
 				'placeholder'=>'10',
-				),								
+				'depends'=>array(array('title', '!=', '')),
+			),
+
 			'text'=>array(
-				'type'=>'editor', 
+				'type'=>'editor',
 				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_IMAGE_CONTENT_CONTENT'),
 				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_IMAGE_CONTENT_CONTENT_DESC'),
-				),
+			),
+
 			//Button
 			'button_text'=>array(
-				'type'=>'text', 
-				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_TEXT'),
-				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_TEXT_DESC'),
+				'type'=>'text',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_TEXT'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_TEXT_DESC'),
 				'std'=>'Button Text',
-				),
-			'button_url'=>array(
-				'type'=>'text', 
-				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_URL'),
-				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_URL_DESC'),
-				'std'=>'http://'
-				),
-			'button_size'=>array(
-				'type'=>'select', 
-				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_SIZE'),
-				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_SIZE_DESC'),
+			),
+
+			'button_fontstyle'=>array(
+				'type'=>'select',
+				'title'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_FONT_STYLE'),
 				'values'=>array(
-					''=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_SIZE_DEFAULT'),
-					'lg'=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_SIZE_LARGE'),
-					'sm'=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_SIZE_SMALL'),
-					'xs'=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_SIZE_EXTRA_SAMLL'),
-					),
+					'underline'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_UNDERLINE'),
+					'uppercase'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_UPPERCASE'),
+					'italic'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_ITALIC'),
+					'lighter'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_LIGHTER'),
+					'normal'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_NORMAL'),
+					'bold'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_BOLD'),
+					'bolder'=> JText::_('COM_SPPAGEBUILDER_GLOBAL_FONT_STYLE_BOLDER'),
 				),
+				'multiple'=>true,
+				'std'=>'',
+				'depends'=> array(
+					array('button_text', '!=', ''),
+				)
+			),
+
+			'button_letterspace'=>array(
+				'type'=>'select',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_LETTER_SPACING'),
+				'values'=>array(
+					'0'=> 'Default',
+					'1px'=> '1px',
+					'2px'=> '2px',
+					'3px'=> '3px',
+					'4px'=> '4px',
+					'5px'=> '5px',
+					'6px'=>	'6px',
+					'7px'=>	'7px',
+					'8px'=>	'8px',
+					'9px'=>	'9px',
+					'10px'=> '10px'
+				),
+				'std'=>'0',
+				'depends'=> array(
+					array('button_text', '!=', ''),
+				)
+			),
+
+			'button_url'=>array(
+				'type'=>'media',
+				'format'=>'attachment',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_URL'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_URL_DESC'),
+				'placeholder'=>'http://',
+				'depends'=> array(
+					array('button_text', '!=', ''),
+				),
+			),
+
+			'button_target'=>array(
+				'type'=>'select',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_LINK_NEWTAB'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_LINK_NEWTAB_DESC'),
+				'values'=>array(
+					''=>JText::_('COM_SPPAGEBUILDER_ADDON_GLOBAL_TARGET_SAME_WINDOW'),
+					'_blank'=>JText::_('COM_SPPAGEBUILDER_ADDON_GLOBAL_TARGET_NEW_WINDOW'),
+				),
+				'depends'=> array(
+					array('button_text', '!=', ''),
+				)
+			),
+
 			'button_type'=>array(
-				'type'=>'select', 
-				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_TYPE'),
-				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_TYPE_DESC'),
+				'type'=>'select',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_STYLE'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_STYLE_DESC'),
 				'values'=>array(
 					'default'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_DEFAULT'),
 					'primary'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_PRIMARY'),
@@ -137,38 +246,145 @@ SpAddonsConfig::addonConfig(
 					'warning'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_WARNING'),
 					'danger'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_DANGER'),
 					'link'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_LINK'),
-					),
+					'custom'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_CUSTOM'),
+				),
 				'std'=>'default',
+				'depends'=> array(
+					array('button_text', '!=', ''),
+				)
+			),
+
+			'button_appearance'=>array(
+				'type'=>'select',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_APPEARANCE'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_APPEARANCE_DESC'),
+				'values'=>array(
+					''=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_APPEARANCE_FLAT'),
+					'outline'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_APPEARANCE_OUTLINE'),
+					'3d'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_APPEARANCE_3D'),
 				),
-			'button_icon'=>array(
-				'type'=>'icon', 
-				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_ICON'),
-				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_BUTTON_ICON_DESC'),
+				'std'=>'flat',
+				'depends'=> array(
+					array('button_text', '!=', ''),
+				)
+			),
+
+			'button_background_color'=>array(
+				'type'=>'color',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_BACKGROUND_COLOR'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_BACKGROUND_COLOR_DESC'),
+				'std' => '#444444',
+				'depends'=> array(
+					array('button_type', '=', 'custom'),
+					array('button_text', '!=', ''),
+				)
+			),
+
+			'button_color'=>array(
+				'type'=>'color',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_COLOR'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_COLOR_DESC'),
+				'std' => '#fff',
+				'depends'=> array(
+					array('button_type', '=', 'custom'),
+					array('button_text', '!=', ''),
+				)
+			),
+
+			'button_background_color_hover'=>array(
+				'type'=>'color',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_BACKGROUND_COLOR_HOVER'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_BACKGROUND_COLOR_HOVER_DESC'),
+				'std' => '#222',
+				'depends'=> array(
+					array('button_type', '=', 'custom'),
+					array('button_text', '!=', ''),
+				)
+			),
+
+			'button_color_hover'=>array(
+				'type'=>'color',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_COLOR_HOVER'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_COLOR_HOVER_DESC'),
+				'std' => '#fff',
+				'depends'=> array(
+					array('button_type', '=', 'custom'),
+					array('button_text', '!=', ''),
+				)
+			),
+
+			'button_size'=>array(
+				'type'=>'select',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_SIZE'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_SIZE_DESC'),
+				'values'=>array(
+					''=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_SIZE_DEFAULT'),
+					'lg'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_SIZE_LARGE'),
+					'xlg'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_SIZE_XLARGE'),
+					'sm'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_SIZE_SMALL'),
+					'xs'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_SIZE_EXTRA_SAMLL'),
 				),
+				'depends'=> array(
+					array('button_text', '!=', ''),
+				)
+			),
+
+			'button_shape'=>array(
+				'type'=>'select',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_SHAPE'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_SHAPE_DESC'),
+				'values'=>array(
+					'rounded'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_SHAPE_ROUNDED'),
+					'square'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_SHAPE_SQUARE'),
+					'round'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_SHAPE_ROUND'),
+				),
+				'depends'=> array(
+					array('button_text', '!=', ''),
+				)
+			),
+
 			'button_block'=>array(
-				'type'=>'select', 
-				'title'=> 'Button Full Width',
-				'desc'=>'Button block',
+				'type'=>'select',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_BLOCK'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_BLOCK_DESC'),
 				'values'=>array(
-					''=> 'No',
-					'sppb-btn-block'=> 'Yes'
-					)
+					''=>JText::_('JNO'),
+					'sppb-btn-block'=>JText::_('JYES'),
 				),
-			'button_target'=>array(
-				'type'=>'select', 
-				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_GLOBAL_TARGET'),
-				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_GLOBAL_TARGET_DESC'),
+				'depends'=> array(
+					array('button_text', '!=', ''),
+				)
+			),
+
+			'button_icon'=>array(
+				'type'=>'icon',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_ICON'),
+				'desc'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_ICON_DESC'),
+				'depends'=> array(
+					array('button_text', '!=', ''),
+				)
+			),
+
+			'button_icon_position'=>array(
+				'type'=>'select',
+				'title'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_BUTTON_ICON_POSITION'),
 				'values'=>array(
-					''=>JText::_('COM_SPPAGEBUILDER_ADDON_GLOBAL_TARGET_SAME_WINDOW'),
-					'_blank'=>JText::_('COM_SPPAGEBUILDER_ADDON_GLOBAL_TARGET_NEW_WINDOW'),
-					),
+					'left'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_LEFT'),
+					'right'=>JText::_('COM_SPPAGEBUILDER_GLOBAL_RIGHT'),
 				),
+				'depends'=> array(
+					array('button_text', '!=', ''),
+				),
+			),
+
 			'class'=>array(
-				'type'=>'text', 
+				'type'=>'text',
 				'title'=>JText::_('COM_SPPAGEBUILDER_ADDON_CLASS'),
 				'desc'=>JText::_('COM_SPPAGEBUILDER_ADDON_CLASS_DESC'),
-				'std'=> ''
-				),
-			)
-		)
-	);
+				'std'=>''
+			),
+
+		),
+	),
+	)
+);
--- template/sppagebuilder/addons/image_content/site.php
+++ template/sppagebuilder/addons/image_content/site.php
@@ -2,90 +2,91 @@
 /**
  * @package SP Page Builder
  * @author JoomShaper http://www.joomshaper.com
- * @copyright Copyright (c) 2010 - 2015 JoomShaper
+ * @copyright Copyright (c) 2010 - 2016 JoomShaper
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or later
 */
 //no direct accees
-defined ('_JEXEC') or die ('resticted aceess');
+defined ('_JEXEC') or die ('restricted aceess');
 
-AddonParser::addAddon('sp_image_content','sp_image_content_addon');
+class SppagebuilderAddonImage_content extends SppagebuilderAddons{
 
-function sp_image_content_addon($atts){
+	public function render() {
+    
+		$class = (isset($this->addon->settings->class) && $this->addon->settings->class) ? $this->addon->settings->class : '';
+		$title = (isset($this->addon->settings->title) && $this->addon->settings->title) ? $this->addon->settings->title : '';
+		$heading_selector = (isset($this->addon->settings->heading_selector) && $this->addon->settings->heading_selector) ? $this->addon->settings->heading_selector : 'h3';
 
-	extract(spAddonAtts(array(
-		"image" 				=> '',
-		"image_width" 			=> '',
-		"image_alignment"		=> '',
-		"title" 				=> '',
-		"heading_selector" 		=> 'h3',
-		"title_fontsize" 		=> '',
-		"title_fontweight" 		=> '',
-		"title_text_color" 		=> '',
-		"title_margin_top" 		=> '',
-		"title_margin_bottom" 	=> '',	
-		"text" 					=> '',
-		"button_text"			=> '',
-		"button_url"			=> '',
-		"button_size"			=> '',
-		"button_type"			=> '',
-		"button_icon"			=> '',
-		"button_block"			=> '',
-		"button_target"			=> '',
-		"class"=>'',
-		), $atts));
+		//Options
+		$image = (isset($this->addon->settings->image) && $this->addon->settings->image) ? $this->addon->settings->image : '';
+		$image_width = (isset($this->addon->settings->image_width) && $this->addon->settings->image_width) ? $this->addon->settings->image_width : '';
+		$image_alignment = (isset($this->addon->settings->image_alignment) && $this->addon->settings->image_alignment) ? $this->addon->settings->image_alignment : '';
+		$text = (isset($this->addon->settings->text) && $this->addon->settings->text) ? $this->addon->settings->text : '';
+		$button_text = (isset($this->addon->settings->button_text) && $this->addon->settings->button_text) ? $this->addon->settings->button_text : '';
+		$button_url = (isset($this->addon->settings->button_url) && $this->addon->settings->button_url) ? $this->addon->settings->button_url : '';
+		$button_classes = (isset($this->addon->settings->button_size) && $this->addon->settings->button_size) ? ' sppb-btn-' . $this->addon->settings->button_size : '';
+		$button_classes .= (isset($this->addon->settings->button_type) && $this->addon->settings->button_type) ? ' sppb-btn-' . $this->addon->settings->button_type : '';
+		$button_classes .= (isset($this->addon->settings->button_shape) && $this->addon->settings->button_shape) ? ' sppb-btn-' . $this->addon->settings->button_shape: ' sppb-btn-rounded';
+		$button_classes .= (isset($this->addon->settings->button_appearance) && $this->addon->settings->button_appearance) ? ' sppb-btn-' . $this->addon->settings->button_appearance : '';
+		$button_classes .= (isset($this->addon->settings->button_block) && $this->addon->settings->button_block) ? ' ' . $this->addon->settings->button_block : '';
+		$button_icon = (isset($this->addon->settings->button_icon) && $this->addon->settings->button_icon) ? $this->addon->settings->button_icon : '';
+		$button_icon_position = (isset($this->addon->settings->button_icon_position) && $this->addon->settings->button_icon_position) ? $this->addon->settings->button_icon_position: 'left';
+		$button_position = (isset($this->addon->settings->button_position) && $this->addon->settings->button_position) ? $this->addon->settings->button_position : '';
+		$button_attribs = (isset($this->addon->settings->button_target) && $this->addon->settings->button_target) ? ' target="' . $this->addon->settings->button_target . '"' : '';
+		$button_attribs .= (isset($this->addon->settings->button_url) && $this->addon->settings->button_url) ? ' href="' . $this->addon->settings->button_url . '"' : '';
 
-
-	if($image_alignment=='left') {
-		$eontent_class = ' sppb-col-sm-offset-6';
-	} else {
-		$eontent_class = '';
-	}
-
-
-	if($image && $title) {
-
-		$output  = '<div class="sppb-addon sppb-addon-image-content aligment-'. $image_alignment .' clearfix ' . $class . '">';
-
-		//Image
-		$output .= '<div style="background-image: url(' .  $image . ');" class="sppb-image-holder">';
-		$output .= '</div>';
-
-		//Content
-		$output .= '<div class="sppb-container">';
-		$output .= '<div class="sppb-row">';
-		
-		$output .= '<div class="sppb-col-sm-6'. $eontent_class .'">';
-		$output .= '<div class="sppb-content-holder">';
-
-		$title_style = '';
-		if($title_margin_top !='') $title_style .= 'margin-top:' . (int) $title_margin_top . 'px;';
-		if($title_margin_bottom !='') $title_style .= 'margin-bottom:' . (int) $title_margin_bottom . 'px;';
-		if($title_text_color) $title_style .= 'color:' . $title_text_color  . ';';
-		if($title_fontsize) $title_style .= 'font-size:'.$title_fontsize.'px;line-height:'.$title_fontsize.'px;';
-		if($title_fontweight) $title_style .= 'font-weight:'.$title_fontweight.';';
-
-		$output .= '<'.$heading_selector.' class="sppb-image-content-title" style="' . $title_style . '">' . $title . '</'.$heading_selector.'>';
-
-		if($text) $output .= '<p class="sppb-image-content-text">' . $text . '</p>';
-
-		if($button_icon) {
-			$button_text = '<i class="fa ' . $button_icon . '"></i> ' . $button_text;
+		if($button_icon_position == 'left') {
+			$button_text = ($button_icon) ? '<i class="fa ' . $button_icon . '"></i> ' . $button_text : $button_text;
+		} else {
+			$button_text = ($button_icon) ? $button_text . ' <i class="fa ' . $button_icon . '"></i>' : $button_text;
+		}
+		$button_output = '';
+		if ($button_text) {
+			$button_output = '<a' . $button_attribs . ' id="btn-'. $this->addon->id .'" class="sppb-btn' . $button_classes . '">' . $button_text . '</a>';
 		}
 
-		if($button_text) {
-			$output .= '<a target="' . $button_target . '" href="' . $button_url . '" class="sppb-btn sppb-btn-' . $button_type . ' sppb-btn-' . $button_size . ' ' . $button_block . '" role="button">' . $button_text . '</a>';
+		if($image_alignment=='left') {
+			$content_class = ' sppb-col-sm-offset-6';
+		} else {
+			$content_class = '';
 		}
 
-		$output .= '</div>';
-		$output .= '</div>';
-		$output .= '</div>';
-		$output .= '</div>';
-		
-		$output .= '</div>';
+		if($image && $text) {
 
-		return $output;
+			$output  = '<div class="sppb-addon sppb-addon-image-content aligment-'. $image_alignment .' clearfix ' . $class . '">';
+
+			//Image
+			$output .= '<div style="background-image: url(' . JURI::base(true) . '/' . $image . ');" class="sppb-image-holder">';
+			$output .= '</div>';
+
+			//Content
+			$output .= '<div class="sppb-container">';
+			$output .= '<div class="sppb-row">';
+
+			$output .= '<div class="sppb-col-sm-6'. $content_class .'">';
+			$output .= '<div class="sppb-content-holder">';
+			$output .= ($title) ? '<'.$heading_selector.' class="sppb-image-content-title">' . $title . '</'.$heading_selector.'>' : '';
+			$output .= ($text) ? '<p class="sppb-image-content-text">' . $text . '</p>' : '';
+
+			$output .= $button_output;
+
+			$output .= '</div>';
+			$output .= '</div>';
+			$output .= '</div>';
+			$output .= '</div>';
+
+			$output .= '</div>';
+
+			return $output;
+		}
+
+		return;
 	}
 
-	return;
+	public function css() {
+		$addon_id = '#sppb-addon-' . $this->addon->id;
+		$layout_path = JPATH_ROOT . '/components/com_sppagebuilder/layouts';
+		$css_path = new JLayoutFile('addon.css.button', $layout_path);
+		return $css_path->render(array('addon_id' => $addon_id, 'options' => $this->addon->settings, 'id' => 'btn-' . $this->addon->id));
+	}
 
 }

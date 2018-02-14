fb5b42bcdc5f27541de2d019880f588391f7d790
Rifat Wahid Alif <rifat3280@gmail.com>
2018-02-14 15:20:27 +0600

    RE: XSS vulnerability in module chromes issue fixed

--- template/html/modules.php
+++ template/html/modules.php
@@ -12,16 +12,14 @@ function modChrome_sp_xhtml($module, $params, $attribs) {
 
 	$moduleTag     = htmlspecialchars($params->get('module_tag', 'div'), ENT_QUOTES, 'UTF-8');
 	$bootstrapSize = (int) $params->get('bootstrap_size', 0);
-	$moduleClass   = $bootstrapSize !== 0 ? ' span' . $bootstrapSize : '';
+	$moduleClass   = $bootstrapSize !== 0 ? ' col-sm-' . $bootstrapSize : '';
 	$headerTag     = htmlspecialchars($params->get('header_tag', 'h3'), ENT_QUOTES, 'UTF-8');
-	$headerClass   = htmlspecialchars($params->get('header_class', 'page-header'), ENT_COMPAT, 'UTF-8');
+	$headerClass   = htmlspecialchars($params->get('header_class', 'sp-module-title'), ENT_COMPAT, 'UTF-8');
 
-	if ($module->content)
-	{
+	if ($module->content) {
 		echo '<' . $moduleTag . ' class="sp-module ' . htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $moduleClass . '">';
-
-			if ($module->showtitle)
-			{
+			
+			if ($module->showtitle) {
 				echo '<' . $headerTag . ' class="' . $headerClass . '">' . $module->title . '</' . $headerTag . '>';
 			}
 

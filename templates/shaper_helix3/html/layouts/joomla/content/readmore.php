49e59ce5c3fcaf95fdbfe084a3ab9dfcea99d4c5
kawshar <kawshar@joomshaper.com>
2021-12-10 14:49:59 +0600

    stable tag

--- templates/shaper_helix3/html/layouts/joomla/content/readmore.php
+++ templates/shaper_helix3/html/layouts/joomla/content/readmore.php
@@ -20,7 +20,7 @@ $item = $displayData['item'];
 				echo JHtml::_('string.truncate', ($item->title), $params->get('readmore_limit'));
 			endif;
 		elseif ($params->get('show_readmore_title', 0) == 0) :
-			echo JText::sprintf('COM_CONTENT_READ_MORE_TITLE');
+			echo JText::sprintf('COM_CONTENT_READ_MORE_TITLE', JHtml::_('string.truncate', ($item->title), $params->get('readmore_limit')));
 		else :
 			echo JText::_('COM_CONTENT_READ_MORE');
 			echo JHtml::_('string.truncate', ($item->title), $params->get('readmore_limit'));

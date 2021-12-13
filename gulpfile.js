0aaa6f157b6cfb67d6821834466d4717834e1c3f
kawshar <kawshar@joomshaper.com>
2021-12-13 13:06:56 +0600

    updated build functionality

--- gulpfile.js
+++ gulpfile.js
@@ -4,41 +4,49 @@ const clean = require("gulp-clean");
 
 // Create a build
 function cleanBuild() {
-	return src("./build", { read: false, allowEmpty: true }).pipe(clean());
+	return src("./dist/build", { read: false, allowEmpty: true }).pipe(clean());
 }
 
 function cleanZip() {
-	return src("./helix3_template.zip", { read: false, allowEmpty: true }).pipe(clean());
+	return src("./dist/**.zip", { read: false, allowEmpty: true }).pipe(clean());
 }
 
 function copy_template() {
-	return src(["./templates/shaper_helix3/**/*.*"]).pipe(dest("build/template"));
+	return src(["./templates/shaper_helix3/**/*.*"]).pipe(dest("dist/build/template"));
 }
 
 function copy_template_lang() {
-	return src("./language/en-GB/en-GB.tpl_shaper_helix3.ini").pipe(dest("build/template"));
+	return src("./language/en-GB/en-GB.tpl_shaper_helix3.ini").pipe(dest("dist/build/template"));
 }
 
 function copy_system_plugin() {
-	return src(["./plugins/system/helix3/**/*.*"]).pipe(dest("build/plugins/system"));
+	return src(["./plugins/system/helix3/**/*.*"]).pipe(dest("dist/build/plugins/system"));
 }
 
 function copy_system_plugin_lang() {
 	return src("./administrator/language/en-GB/en-GB.plg_system_helix3.ini").pipe(
-		dest("build/plugins/system/language")
+		dest("dist/build/plugins/system/language")
 	);
 }
 
 function copy_ajax_plugin() {
-	return src(["./plugins/ajax/helix3/**/*.*"]).pipe(dest("build/plugins/ajax"));
+	return src(["./plugins/ajax/helix3/**/*.*"]).pipe(dest("dist/build/plugins/ajax"));
 }
 
 function copy_installer() {
-	return src(["installer.script.php", "installer.xml"]).pipe(dest("build"));
+	return src(["installer.script.php", "installer.xml"]).pipe(dest("dist/build"));
 }
 
-function makeZip() {
-	return src("./build/**/*.*").pipe(zip("helix3_template.zip")).pipe(dest("./"));
+function makeTemplateZip() {
+	return src("./dist/build/**/*.*").pipe(zip("helix3_template.zip")).pipe(dest("./dist/"));
+}
+
+function makeSystemPluginZip() {
+	return src("./dist/build/plugins/system/**/*.*").pipe(zip("plg_system_helix.zip")).pipe(dest("./dist/"));
+}
+
+function makeAjaxPluginZip() {
+	return src("./dist/build/plugins/ajax/**/*.*").pipe(zip("plg_ajax_helix.zip")).pipe(dest("./dist/"));
 }
 
 exports.copy = series(
@@ -51,4 +59,5 @@ exports.copy = series(
 	copy_ajax_plugin,
 	copy_installer
 );
-exports.default = series(exports.copy, makeZip, cleanBuild);
+
+exports.default = series(exports.copy, makeSystemPluginZip, makeAjaxPluginZip, makeTemplateZip, cleanBuild);
